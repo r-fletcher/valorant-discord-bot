3369288663dedaa05847ad3fb6990d556ceb2367
r-fletcher <fletcher.ryj@gmail.com>
2026-03-23 13:24:20 +0200

    Added mmr change for history.

--- commands/history.js
+++ commands/history.js
@@ -51,9 +51,20 @@ module.exports = {
                 { headers: { Authorization: process.env.VAL_API_KEY } }
             );
 
-            const matches = res.data.data.slice(0, 5);
+            const mmr = await axios.get(
+                `${BASE_URL}/valorant/v1/mmr-history/${REGION}/${name}/${tag}`,
+                { headers: { Authorization: process.env.VAL_API_KEY } }
+            )
 
-            const matchEmbeds = matches.map(match => {
+            matchesData = res.data.data.slice(0, 5);
+            mmrData = mmr.data.data.slice(0, 5);
+
+            const combinedMatches = matchesData.map((match, i) => ({
+                ...match,
+                mmr: mmrData[i] || null
+            }));
+
+            const matchEmbeds = combinedMatches.map(match => {
                 const player = match.players.all_players.find(
                     p => p.name.replaceAll(' ', '') === name.replaceAll(' ','') && p.tag === tag
                 );
@@ -71,6 +82,14 @@ module.exports = {
                 const draw = match.teams.blue.has_won === match.teams.red.has_won;
                 const win = match.teams[team]?.has_won ?? false;
 
+                const rrChange = match.mmr?.mmr_change_to_last_game;
+                const rrText =
+                    rrChange === undefined
+                        ? 'N/A'
+                        : rrChange > 0
+                        ? `+${rrChange}`
+                        : `${rrChange}`;
+
                 return new EmbedBuilder()
                 .setTitle(`**${map}**`)
                 .setColor(draw ? 0xd2d5d8 : win ? 0x00ff88 : 0xff4655)
@@ -79,7 +98,8 @@ module.exports = {
                 .addFields(
                     { name: 'Agent', value: agent, inline: true },
                     { name: 'KDA', value: `${kills}/${deaths}/${assists}`, inline: true },
-                    { name: 'K/D', value: kd, inline: true }
+                    { name: 'K/D', value: kd, inline: true },
+                    { name: 'RR Change', value: rrText, inline: true}
                 );
             });
 
