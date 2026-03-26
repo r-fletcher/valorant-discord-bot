57ef400f07f062d1e728abe597baa16469b27f79
r-fletcher <fletcher.ryj@gmail.com>
2026-03-26 17:13:05 +0200

    Moved source files to to src/

---
+++ src/commands/stats.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+const {EmbedBuilder} = require('discord.js');
+const db = require('../db');
+const cache = require('../cache');
+
+const REGION = 'eu';
+const BASE_URL = 'https://api.henrikdev.xyz';
